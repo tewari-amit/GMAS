66a28a33531faf41f06faf8b95514505e7f7b23c
TIMESGROUP\Amit.Tewari <amit.tewari@timesinternet.in>
2018-01-31 02:59:05 +0530

    2nd commit

--- src/test/java/artifactid/gmas/LoginTest.java
+++ src/test/java/artifactid/gmas/LoginTest.java
@@ -1,6 +1,7 @@
 package artifactid.gmas;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
@@ -21,7 +22,8 @@ public class LoginTest {
 		driver.get("http://teststore2.getmeashop.com/user/login/");
 		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/p[1]/input")).sendKeys("4mail2amit@gmail.com");
 		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/p[2]/input")).sendKeys("test1");
-		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/button")).click();
+		//driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/button")).click();
+		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/button")).sendKeys(Keys.ENTER);
 	}
 	
 	@AfterTest
