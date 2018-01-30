14e5c8b7692cbfe8079d331d69c000664b831c1f
TIMESGROUP\Amit.Tewari <amit.tewari@timesinternet.in>
2018-01-31 02:41:42 +0530

    1st commit of enduser login

---
+++ src/test/java/artifactid/gmas/LoginTest.java
@@ -0,0 +1,32 @@
+package artifactid.gmas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://teststore2.getmeashop.com/user/login/");
+		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/p[1]/input")).sendKeys("4mail2amit@gmail.com");
+		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/p[2]/input")).sendKeys("test1");
+		driver.findElement(By.xpath("//section/div/div/div/div/div[1]/div/form/button")).click();
+	}
+	
+	@AfterTest
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
